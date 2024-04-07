25c945195092bfc5111f2289d4868a6025639d65
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2024-04-07 11:23:48 +0700

    feat get publisher update
    updating the get publisher feature with fixing some major bug

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -20,16 +20,37 @@ class PublisherController extends Controller
             ], 500);
         }
     }
-    public function get(Request $request, $name) {
-        // $payload = $request->input('name');
+    public function get(Request $request, $name = null) {
         try {
-            Publisher::where('name', $name)->first;
-        } catch (\Throwable $th) {
+            if ($request->has('name')) {
+                $payload = $request->query('name');
+                $data = Publisher::where('name', $payload)->first();
+            } elseif ($name !== null) {
+                $data = Publisher::where('name', $name)->first();
+            } else {
+                // Handle case where both $request and $name are null
+                throw new \InvalidArgumentException("No name parameter provided.");
+            }
+
+            if ($data) {
+                return response()->json([
+                    "code" => 200,
+                    "message" => "success",
+                    "data" => $data
+                ], 200);
+            } else {
+                return response()->json([
+                    "code" => 404,
+                    "message" => "Not found",
+                ], 404);
+            }
+        } catch (\Exception $exception) {
             return response()->json([
                 "code" => 500,
                 "message" => "fail",
-                "error" => $exceptions
+                "error" => $exception->getMessage()
             ], 500);
         }
     }
+
 }
